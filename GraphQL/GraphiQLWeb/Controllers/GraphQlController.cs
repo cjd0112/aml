50f0fe8c821ab5e659d9b80267650266ecdc866b
cjd0112 <colin.dick1@btinternet.com>
2017-11-29 08:26:28 +0000

    xxx

---
+++ GraphQL/GraphiQLWeb/Controllers/GraphQlController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphiQLWeb.Models;
+using GraphQL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GraphiQLWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("GraphQl")]
+    public class GraphQlController : Controller
+    {
+        public class SubObject
+        {
+            public enum Blah
+            {
+                Blah1,
+                Blah2,
+            }
+
+            public String TEst2 { get; set; }
+            public Blah Foo { get; set; }
+        }
+        public class Query
+        {
+            public Query()
+            {
+                foo = new List<SubObject>();
+                foo.Add(new SubObject { Foo = SubObject.Blah.Blah1, TEst2 = "hallo flldldl" });
+                foo.Add(new SubObject { Foo = SubObject.Blah.Blah1, TEst2 = "hallo flldldl" });
+                foo.Add(new SubObject { Foo = SubObject.Blah.Blah1, TEst2 = "hallo flldldl" });
+
+            }
+            public List<SubObject> foo { get; set; }
+
+
+            public SubObject MySubObject { get; set; }
+
+
+
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] GraphQlQuery query)
+        {
+            var output = new GraphQlDocument(query.Query)
+                .Validate(typeof(Query))
+                .Run(new Query())
+                .GetOutput();
+
+            var g = Directory.GetCurrentDirectory();
+
+            var s = new StreamReader("TestSchema.json").ReadToEnd();
+
+            var obj = JObject.Parse(output);
+
+
+            return Ok(obj);
+
+        }
+    }
+}
