a03da8dfd82ccd4fbe688cc6e668dc1088632686
cjd0112 <colin.dick@alphastorm.co.uk>
2017-12-03 20:25:34 +0000

    sss

---
+++ DistributedAML/AMLWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AMLWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
