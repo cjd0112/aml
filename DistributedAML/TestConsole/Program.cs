71ae31b4abbe545965abec7b185d49a541c3f6ea
Colin <colin.dick@alphastorm.co.uk>
2017-12-24 18:04:25 +0000

    hello

--- DistributedAML/TestConsole/Program.cs
+++ DistributedAML/TestConsole/Program.cs
@@ -9,6 +9,7 @@ using Fasterflect;
 using GraphQL;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace TestConsole
 {
@@ -20,11 +21,34 @@ namespace TestConsole
             public String cat { get; set; }
         }
 
+        public static IRestResponse SendSimpleMessage()
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator =
+            new RestSharp.Authenticators.HttpBasicAuthenticator("api",
+                                      "key-b74d7782c1dc73f525a9c537e2a6e9b8");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "sandboxbce8c31fad2d4da1b64304289c3c2338.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Mailgun Sandbox <postmaster@sandboxbce8c31fad2d4da1b64304289c3c2338.mailgun.org>");
+            request.AddParameter("to", "Colin <colin.dick@alphastorm.co.uk>");
+            request.AddParameter("subject", "Hello Colin");
+            request.AddParameter("text", "Congratulations Colin, you just sent an email with Mailgun!  You are truly awesome!");
+            request.Method = Method.POST;
+            return client.Execute(request);
+        }
+
+
         static void Main(string[] args)
         {
             
             try
             {
+
+                var z22 = SendSimpleMessage();
+               
+
                 var definition = new
                 {
                     data = new
